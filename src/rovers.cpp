99b6fc7cc33bd25ded74fb2f4bcc4c68fc6a8f4f
Daniel Casimir Grigg <mail@danielgrigg.com>
2012-11-04 16:30:48 +1000

    Add file input. Version bump.

--- src/rovers.cpp
+++ src/rovers.cpp
@@ -7,7 +7,7 @@
 #include <sstream>
 #include <string>
 #include <boost/program_options.hpp>
-
+#include <fstream>
 
 using namespace std::tr1;
 
@@ -19,29 +19,32 @@ std::string version_string() {
   return ss.str();
 }
 
+const char* EXAMPLE_INPUT = "    5 5\n"
+                            "    1 2 N\n"
+                            "    LMLMLMLMM\n"
+                            "    3 3 E\n"
+                            "    MMRMMRMRRM\n"
+                            "    ^D\n";
+
 std::string usage_string() {
   std::stringstream ss;
   ss << "Usage\n\n"
     << "  rovers [options]\n\n"
     << "Running\n\n"
-    << "  TODO\n";
-  return ss.str();
-}
-
-std::string example_input() {
-  return "5 5\n"
-    "1 2 N\n"
-    "LMLMLMLMM\n"
-    "3 3 E\n"
-    "MMRMMRMRRM";
+    << "  With no arguments specified, rovers reads simulation input from"
+    << " stdin.\n"
+    << "  An example session of 2 rovers on a 6x6 plateau:\n"
+    << EXAMPLE_INPUT
+    << "  Where ^D signals end-of-input.\n";
+    return ss.str();
 }
 
 const std::string UNKNOWN_ERROR = 
-  "rovers has encountered an unknown error and must exit";
+"rovers has encountered an unknown error and must exit";
 
 const std::string INVALID_INPUT_ERROR = 
-  "Invalid input format.  Read problem_description.txt for "
-  "specs or run with --help for an example.";
+"Invalid input format.  Read problem_description.txt for "
+"specs or run with --help for an example.";
 
 
 int main(int argc, char* argv[]) {
@@ -50,19 +53,21 @@ int main(int argc, char* argv[]) {
 
   namespace po = boost::program_options;
 
-  // We only have one option atm, but '--help' is universally expected for
-  // command-line utils.
+  std::string infile;
   try {
     po::options_description desc("Available options");
     desc.add_options()
-      ("help", "display help message");
+      ("help", "display help message")
+      ("file", po::value<std::string>(), "read simulation input from a file");
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
     if (vm.count("help")) {
       cout << version_string() << "\n" << usage_string() << "\n" << desc 
-           << endl;
+        << endl;
       return 0;
+    } else if (vm.count("file")) {
+      infile = vm["file"].as<std::string>();
     }
   } catch (std::exception& e) {
     std::cerr << e.what() << endl;
@@ -72,32 +77,44 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
+  std::ifstream ifs;
+  std::streambuf* cin_buf = NULL;
+
+  // Redirect std::cin to read from a file if the user wants it.
+  if (!infile.empty()) {
+    ifs.open(infile.c_str());
+    if (!ifs.good()) {
+      std::cerr << "Unable to read input file\n";
+      return 1;
+    }
+    cin_buf = std::cin.rdbuf();
+    std::cin.rdbuf(ifs.rdbuf());
+  }
+
   std::string upper_right;
   std::vector<RoverCommand> rover_commands;
-
   if (parse_rover_world(std::cin, upper_right, rover_commands)) {
-
     try {
       std::vector<rv::Rover> squad;
       simulate_squad(rover_commands, upper_right, squad);
 
+      // Write each rover's position
       std::copy(squad.begin(), squad.end(), 
           std::ostream_iterator<rv::Rover>(std::cout, "\n"));
     }
     catch (std::invalid_argument e) {
       std::cerr << "Simulation aborted: " << e.what() << endl;
-      return 1;
     }
     catch (...) {
       std::cerr << UNKNOWN_ERROR << endl;
-      return 1;
     }
 
   } else {
     std::cerr << INVALID_INPUT_ERROR << endl;
-    return 1;
   }
 
+  if (cin_buf) std::cin.rdbuf(cin_buf);
+  ifs.close();
   return 0;
 }
 
