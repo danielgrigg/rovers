97e8a17803359a4c984916d66a178f5766f28268
Daniel Casimir Grigg <mail@danielgrigg.com>
2012-11-04 12:44:57 +1000

    Add skeleton command line parsing.

--- src/rovers.cpp
+++ src/rovers.cpp
@@ -1,12 +1,11 @@
 #include "rover.h"
+#include "plateau.h"
 #include "config.h"
 
 #include <iostream>
 #include <sstream>
 #include <string>
-
-using std::cout;
-using std::endl;
+#include <boost/program_options.hpp>
 
 // Version pulled from cmake                                                    
 std::string version_string() {
@@ -16,9 +15,44 @@ std::string version_string() {
   return ss.str();
 }
 
-int main(int argc, char* argv[]) {
+std::string usage_string() {
+  std::stringstream ss;
+  ss << "Usage\n\n"
+    << "  rovers [options]\n\n"
+    << "Running\n\n"
+    << "  TODO\n";
+  return ss.str();
+}
 
-  cout << version_string() << endl;
+int main(int argc, char* argv[]) {
+  using namespace rv;
+  using namespace std;
+
+  namespace po = boost::program_options;
+
+  // We only have one option atm, but '--help' is universally expected for
+  // command-line utils.
+  try {
+    po::options_description desc("Available options");
+    desc.add_options()
+      ("help", "display help message");
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+    if (vm.count("help")) {
+      cout << version_string() << "\n" << usage_string() << "\n" << desc 
+           << endl;
+      return 0;
+    }
+  } catch (std::exception& e) {
+    std::cerr << e.what() << endl;
+    return 1;
+  } catch (...) {
+    std::cerr << "rovers has encountered an unknown error " 
+      << " and must exit." << std::endl;
+    return 1;
+  }
+
   return 0;
 }
 
